806f6f8fcb4f9955ad60ffbfe09c96eb0d133ae1
NamesMT <dangquoctrung123@gmail.com>
2024-04-13 11:36:51 +0000

    chore: coming soon

---
+++ apps/frontend/pages/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { Gradient } from '@local/common'
+
+const colorMode = useColorMode()
+</script>
+
+<template>
+  <div class="h-screen flex justify-center items-center">
+    <Gradient class="logoGradient absolute" :conic="true" />
+
+    <div class="flex flex-col gap-4 prose items-center">
+      <h1>Coming Soon!</h1>
+      <div class="flex gap-2 items-center">
+        <div>Theme:&nbsp;</div>
+        <Button
+          :label="colorMode.preference"
+          @click="colorMode.preference = (colorMode.preference !== 'dark')
+            ? 'dark'
+            : 'light'"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+@import url("~/assets/css/main.css");
+</style>
