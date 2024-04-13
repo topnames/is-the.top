4d72991eb23f07035b0ddb80aebfe99aab0c2e96
NamesMT <dangquoctrung123@gmail.com>
2024-04-13 11:37:02 +0000

    feat: add `anime` utils

---
+++ apps/frontend/utils/anime.ts
@@ -0,0 +1,38 @@
+const smoothSeekInstanceMap = new WeakMap<WeakKey, {
+  targetTime: number
+  seekId: number | undefined
+  seek: (id: number) => void
+}>()
+export function smoothSeek(i: anime.AnimeInstance, seekTo: number) {
+  if (!smoothSeekInstanceMap.get(i)) {
+    smoothSeekInstanceMap.set(i, {
+      targetTime: seekTo,
+      seekId: undefined,
+      seek(id: number) {
+        if (this.seekId && id !== this.seekId)
+          return
+
+        this.seekId = id
+
+        if (i.currentTime !== this.targetTime) {
+          if (i.currentTime < this.targetTime)
+            i.seek(Math.max(Math.min((this.targetTime - i.currentTime) / 20, 25), 0.05) + i.currentTime)
+          else if (i.currentTime > this.targetTime)
+            i.seek(i.currentTime - Math.max(Math.min((i.currentTime - this.targetTime) / 20, 25), 0.05))
+        }
+
+        if (i.currentTime !== this.targetTime)
+          requestAnimationFrame(() => this.seek(id))
+        else
+          this.seekId = undefined
+      },
+    })
+  }
+
+  const obj = smoothSeekInstanceMap.get(i)!
+
+  obj.targetTime = seekTo
+
+  if (!obj.seekId)
+    obj.seek(Math.random())
+}
