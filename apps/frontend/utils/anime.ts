6b67b11a693269048f52ad41f97f75f74e6fd5e1
NamesMT <dangquoctrung123@gmail.com>
2024-04-25 13:36:42 +0000

    feat(anime): `smoothSeek()`: speed, deltaTime support

--- apps/frontend/utils/anime.ts
+++ apps/frontend/utils/anime.ts
@@ -1,36 +1,42 @@
-import { clamp } from '@vueuse/core'
-
 const smoothSeekInstanceMap = new WeakMap<WeakKey, {
   targetTime: number
-  seekId: number | undefined
+  seekId: number | undefined // This ensure that we only have one active loop for each instance
+  elapseStamp: number
   seek: (id: number) => void
 }>()
-export function smoothSeek(i: anime.AnimeInstance, seekTo: number, { clampMin = 0.05, clampMax = 73 } = {}) {
+export function smoothSeek(i: anime.AnimeInstance, seekTo: number, { speed = 0.005, clampMin = 0.05, clampMax = 73 } = {}) {
   if (!smoothSeekInstanceMap.get(i)) {
     smoothSeekInstanceMap.set(i, {
       targetTime: seekTo,
+      elapseStamp: 0,
       seekId: undefined,
       seek(id: number) {
-        if (this.seekId && id !== this.seekId)
-          return
+        requestAnimationFrame((elapsed) => {
+          // Checking
+          if (!this.seekId)
+            this.seekId = id
+          else if (id !== this.seekId)
+            return
 
-        this.seekId = id
-
-        if (i.currentTime !== this.targetTime) {
-          if (i.currentTime < this.targetTime) {
-            const diff = this.targetTime - i.currentTime
-            i.seek(i.currentTime + clamp(diff / 20, clampMin, clampMax))
+          // Logic
+          if (i.currentTime !== this.targetTime) {
+            const seekDestination = easeToTarget(i.currentTime, this.targetTime, {
+              speed,
+              delta: elapsed - this.elapseStamp,
+              clampMin,
+              clampMax,
+            })
+            i.seek(seekDestination)
           }
-          else if (i.currentTime > this.targetTime) {
-            const diff = i.currentTime - this.targetTime
-            i.seek(i.currentTime - clamp(diff / 20, clampMin, clampMax))
-          }
-        }
 
-        if (i.currentTime !== this.targetTime)
-          requestAnimationFrame(() => this.seek(id))
-        else
-          this.seekId = undefined
+          // Cleaning up
+          this.elapseStamp = elapsed
+
+          if (i.currentTime !== this.targetTime)
+            return this.seek(id)
+          else
+            this.seekId = undefined
+        })
       },
     })
   }
@@ -39,6 +45,8 @@ export function smoothSeek(i: anime.AnimeInstance, seekTo: number, { clampMin = 
 
   obj.targetTime = seekTo
 
-  if (!obj.seekId)
+  if (!obj.seekId) {
+    obj.elapseStamp = performance.now()
     obj.seek(Math.random())
+  }
 }
