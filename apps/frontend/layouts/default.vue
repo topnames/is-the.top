43433d4614bae1cbe74b6fdec90b4895a3020d97
NamesMT <dangquoctrung123@gmail.com>
2024-04-25 09:00:09 +0000

    feat: setting up the layout

---
+++ apps/frontend/layouts/default.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+const colorMode = useColorMode()
+</script>
+
+<template>
+  <!-- div for any global style of layout -->
+  <div class="min-h-screen flex flex-col">
+    <header class="h-20 flex items-center justify-between bg-white p-4 [&>div]:(basis-1/3) dark:(bg-gray-8)">
+      <div class="flex items-center justify-start gap-2 text-xl">
+        <div i-tabler:icons />
+        <div>
+          Topnames
+        </div>
+      </div>
+
+      <div class="flex justify-center divide-x divide-primary-2 [&>div]:(px-8)">
+        <div>Home</div>
+        <div>Product</div>
+        <div>Team</div>
+      </div>
+
+      <div class="flex items-center justify-end gap-2">
+        <Button
+          :icon="`shrink-0 ${colorMode.preference !== 'dark' ? `i-tabler:moon` : 'i-tabler:sun'}`"
+          class="h-12 w-12"
+          rounded
+          @click="colorMode.preference = (colorMode.preference !== 'dark')
+            ? 'dark'
+            : 'light'"
+        />
+        <NuxtLink to="https://github.com/topnames/is-the.top" target="_blank">
+          <Button
+            icon="i-tabler:brand-github-filled shrink-0"
+            class="h-12 w-12"
+            rounded
+          />
+        </NuxtLink>
+      </div>
+    </header>
+
+    <main class="grow">
+      <slot />
+    </main>
+
+    <footer class="h-20 flex items-center justify-between bg-white p-4 dark:(bg-gray-8)">
+      <div class="flex items-center justify-start gap-2 text-xl">
+        <div i-tabler:icons />
+        <div>
+          Topnames
+        </div>
+      </div>
+    </footer>
+  </div>
+</template>
--- apps/frontend/pages/index.vue
+++ apps/frontend/pages/index.vue
@@ -1,63 +1,8 @@
 <script setup lang="ts">
-const colorMode = useColorMode()
-const { $anime } = useNuxtApp()
-
-onMounted(async () => {
-  await nextTick()
-  const csEl = document.getElementById('csEl')!
-
-  const csAnime = $anime({
-    targets: csEl.querySelectorAll('.GridMaker__col'),
-    translateX: $anime.stagger([-120, 240], { grid: [48, 6], from: 'center', axis: 'x' }),
-    translateY: () => $anime.random(-600, 1200),
-    rotateZ: () => $anime.random(-90, 90),
-    delay: $anime.stagger(50, { grid: [6, 6], from: 'last' }),
-    easing: 'easeInOutElastic',
-    autoplay: false,
-  })
-
-  usePageScrollPercentage(
-    (p) => {
-      smoothSeek(csAnime, ((p - 1) * -1) * csAnime.duration)
-    },
-    { startOffset: window.innerHeight * 2 / 3 },
-  )
-})
 </script>
 
 <template>
-  <div class="h-screen flex justify-center overflow-x-clip">
-    <div class="relative h-4096px flex flex-col items-center">
-      <div class="m-4 flex items-center gap-4">
-        <div class="flex items-center gap-2">
-          <div class="text-xl">
-            Theme:
-          </div>
-          <div class="flex items-center gap-2">
-            <Button
-              :label="colorMode.preference"
-              @click="colorMode.preference = (colorMode.preference !== 'dark')
-                ? 'dark'
-                : 'light'"
-            />
-          </div>
-        </div>
-
-        <NuxtLink to="https://github.com/topnames/is-the.top" target="_blank">
-          <Button
-            icon="i-tabler:brand-github-filled shrink-0"
-            class="h-12 w-12"
-            text rounded
-          />
-        </NuxtLink>
-      </div>
-
-      <h1 class="sticky top-400px drop-shadow-md light:text-primary-600">
-        <ComingSoon
-          id="csEl"
-          class="[&_.GridMaker\_\_col]:(pointer-events-none will-change-transform)"
-        />
-      </h1>
-    </div>
+  <div class="flex items-center justify-center py-4">
+    <Button label="@topnames" text />
   </div>
 </template>
