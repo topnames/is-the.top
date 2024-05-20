1f8a5ab03da6d3ae6dea926613ff81162b7830a3
NamesMT <dangquoctrung123@gmail.com>
2024-05-20 02:50:43 +0000

    style: remove active `ring` and change img

--- apps/frontend/app/components/section-project/ProjectCarouselItem.vue
+++ apps/frontend/app/components/section-project/ProjectCarouselItem.vue
@@ -2,9 +2,9 @@
 </script>
 
 <template>
-  <div tabindex="0" class="group h-120 w-155 focus-within:ring hover:ring">
+  <div tabindex="0" class="group h-120 w-155">
     <div class="mb-2.5 h-96">
-      <NuxtImg src="https://i.pravatar.cc/300?u=CVArt" class="h-full w-full rounded-16px object-cover" />
+      <NuxtImg src="https://images.unsplash.com/photo-1560461396-ec0ef7bb29dd?q=80&w=2564&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D" class="h-full w-full rounded-16px object-cover" />
     </div>
 
     <div class="flex flex-col gap-2 px-5">
